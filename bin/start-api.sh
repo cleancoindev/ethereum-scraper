1ce87461ff24ad3d8a75638d989a9f1afb26a437
|M| <manicprone@gmail.com>
2021-08-20 18:38:23 -0600

    Dockerizing service

---
+++ bin/start-api.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+echo "--------------------------------"
+echo "ENV_ALIAS: ${ENV_ALIAS}"
+echo "--------------------------------"
+echo ""
+
+echo "Starting Ethereum Scraper API..."
+
+npm run start
